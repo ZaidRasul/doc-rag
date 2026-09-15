12d06fa5f6193636d7aede1639cdc0862ebd08d4
Zaid Rasool <zaidrasool56@gmail.com>
2026-09-15 17:53:46 +0500

    load files function

--- app/document_loader.py
+++ app/document_loader.py
@@ -1,7 +1,34 @@
 import os
-import tempfile
 from pathlib import Path
 from typing import List
-
 from langchain_core.documents import Document
 from langchain_community.document_loaders import PyMuPDFLoader
+
+supported_extensions = [".pdf", ".txt", ".md"]
+
+def load_files(filename: str, file_bytes: bytes) -> List[Document]:
+
+    extension = Path(filename).suffix.lower()
+    if extension not in supported_extensions:
+        raise ValueError(f"Unsupported file type {extension}")
+
+    if extension == ".pdf":
+        return load_pdf(file_name, file_bytes)
+    if extension == ".txt":
+        return load_txt(file_name, file_bytes)
+    if extension == ".md":
+        return load_md(file_name, file_bytes)
+
+
+def load_pdf(file_name: str, file_bytes: bytes) -> List[Document]:
+    pass
+
+
+
+def load_txt(file_name: str, file_bytes: bytes) -> List[Document]:
+    pass
+
+
+
+def load_md(file_name: str, file_bytes: bytes) -> List[Document]:
+    pass
